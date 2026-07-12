221268c7429cf28f6732469b555cf518a28eec99
sammahan-005 <nitoueksamy@gmail.com>
2026-07-12 14:46:04 +0100

    Updates

--- database/seeders/admin_seeder.ts
+++ database/seeders/admin_seeder.ts
@@ -4,8 +4,8 @@ import env from '#start/env'
 
 export default class extends BaseSeeder {
   async run() {
-    const email = env.get('ADMIN_EMAIL') || 'admin@polypress.com'
-    const password = env.get('ADMIN_PASSWORD') || 'SuperSecureAdminPassword123!'
+    const email = env.get('ADMIN_EMAIL')
+    const password = env.get('ADMIN_PASSWORD')
 
     await User.updateOrCreate(
       { email },
